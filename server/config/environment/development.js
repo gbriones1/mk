c2e4588a2ddb4657275b6db43e62eee40ed28647
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2014-12-30 19:16:33 -0600

    Mongolab reference

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,12 +5,12 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    // uri: 'mongodb://localhost/mk-dev',
-    uri: 'mongodb://ds029831.mongolab.com:29831/mk',
-    options: {
-      user: 'admin',
-      pass: 'admin'
-    }
+    uri: 'mongodb://localhost/mk-dev',
+ //    uri: 'mongodb://admin:admin@ds029831.mongolab.com:29831/mk',
+ //    options: {
+	//   server: { socketOptions: { keepAlive: 1 } },
+	//   replset: { socketOptions: { keepAlive: 1 } }
+	// }
   },
 
   seedDB: true
