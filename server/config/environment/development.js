117a6c24487abc83f59ba44e14e4e5849c875aa8
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2014-12-30 18:25:41 -0600

    DB in mongolab

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,12 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/mk-dev'
+    // uri: 'mongodb://localhost/mk-dev',
+    uri: 'mongodb://ds029831.mongolab.com:29831/mk',
+    options: {
+      user: 'admin',
+      pass: 'admin'
+    }
   },
 
   seedDB: true
